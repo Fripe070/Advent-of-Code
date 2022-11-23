5c784f0a177116ef32dfbc405919627dd6a044c4
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2022-11-23 18:06:45 +0100

    Day 3 part 1

--- 2021/day3/part1/part1.py
+++ 2021/day3/part1/part1.py
@@ -2,9 +2,22 @@
 # https://adventofcode.com/2021/day/3
 
 with open("input.txt", "r") as f:
-    ones = 0
-    zeros = 0
-    for line in f.readlines():
-        print(int(line[:1]))
+    bits = [
+        [int(bit) * 2 - 1 for bit in line.strip()]  # 0 becomes -1 and 1 stays as 1
+        for line in f
+    ]
 
+newbits = [0] * len(bits[0])
+for line in bits:
+    for index, value in enumerate(line):
+        newbits[index] += value
 
+gamma = int("".join([str(int(bit > 0)) for bit in newbits]), 2)
+epsilon = int("".join([str(int(bit < 0)) for bit in newbits]), 2)
+
+print(f"Gamma rate: {gamma}")
+print(f"Epsilon rate: {epsilon}")
+
+final = gamma * epsilon
+
+print(f"Power consumption of the submarine: {final}")
