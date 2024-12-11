2d41501d84a2d0727cd8dbefbb32c52211865d5d
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2024-12-11 16:51:27 +0100

    Day 10 part 2

---
+++ 2024/day10/part2.py
@@ -0,0 +1,33 @@
+def ff(grid, x, y, n) -> list[tuple[int, int]]:
+    ends_found = list()
+    for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+        if not (0 <= (n_y := y+dy) < len(grid)): continue
+        if not (0 <= (n_x := x+dx) < len(grid[n_y])): continue
+        if grid[n_y][n_x] == n:
+            if n == 9:
+                ends_found.append((n_x, n_y))
+            else:
+                ends_found.extend(ff(grid, n_x, n_y, n+1))
+    return ends_found
+
+
+def main():
+    with open("input.txt") as file:
+        grid: list[list[int]] = [
+            [int(c) for c in l]
+            for l in file.read().rstrip().splitlines()
+        ]
+
+    ends_found = 0
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            num: int = grid[y][x]
+            if num != 0: continue
+            ends = ff(grid, x, y, 1)
+            ends_found += len(ends)
+
+    print(ends_found)
+
+
+if __name__ == "__main__":
+    main()
